e1e1dff60eb3329a6ccfbe10902ad448afe8383b
dragosticlea5 <dragosionut5.td@gmail.com>
2024-09-09 17:32:13 +0300

    init commit

--- src/main/java/com/example/sports_store_management_main/persistence/repository/ProductRepository.java
+++ src/main/java/com/example/sports_store_management_main/persistence/repository/ProductRepository.java
@@ -15,4 +15,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     //Query to find products with a price below a value
     @Query("SELECT p FROM Product p WHERE p.price <= ?1")
     List<Product> findProductsCheaperThan(Double price);
+
+    @Query(value = "SELECT * FROM products WHERE stock_quantity < ?1", nativeQuery = true)
+    List<Product> findProductsWithLowStock(int stockQuantity);
 }
